34ebbfd60038f26ec90f9b7807e24886cc4b9968
Charles Chen <toshuaijun@gmail.com>
2017-10-28 21:33:00 +0800

    Class访问权限设置

--- basic/Student.py
+++ basic/Student.py
@@ -3,16 +3,16 @@
 
 class Student(object):
 	def __init__(self, name, score):
-		self.name = name
-		self.score = score
+		self.__name = name
+		self.__score = score
 
 	def print_score(self):
-		print '%s %s' % (self.name, self.score)
+		print '%s %s' % (self.__name, self.__score)
 
 	def get_grade(self):
-		if self.score >= 90:
+		if self.__score >= 90:
 			return 'A'
-		elif self.score >= 60:
+		elif self.__score >= 60:
 			return 'B'
 		else:
 			return 'C'
@@ -22,4 +22,8 @@ bart = Student('Bart Zhang', 78)
 # print bart.score
 bart.print_score()
 print bart.get_grade()
+
+# print bart.score
+# bart.score = 90
+# print bart.score
 
